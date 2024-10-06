8f57641f3fefa32aff75275dda5f2397a3dfa49e
Shanu <shanu@Shanus-MacBook-Air.local>
2024-10-06 15:43:08 +0530

    feat: added skip fn dynamically backend

--- src/Pagination/index.jsx
+++ src/Pagination/index.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
+const ITEM_PER_PAGE = 15
 
 export default function Pagination() {
 
     const [prodData, setProdData] = useState([])
     const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(0)
 
     const fetchData = async () => {
-        const res = await fetch("https://dummyjson.com/products?limit=20")
+        const res = await fetch(`https://dummyjson.com/products?limit=${ITEM_PER_PAGE}&skip=${page * ITEM_PER_PAGE - ITEM_PER_PAGE}`)
         const data = await res.json()
+        setTotal(Math.ceil(data.total / ITEM_PER_PAGE))
         setProdData(data.products)
     }
 
@@ -19,10 +22,14 @@ export default function Pagination() {
         setPage(selectedPage)
     }
 
+    if (total === 0) {
+        return <h1>Products loading...</h1>
+    }
+
     return (
         <>
             <div style={{ display: "flex", height: "100%", flexWrap: "wrap" }}>
-                {prodData.slice((page * 5) - 5, page * 5).map(data => {
+                {prodData.map(data => {
                     return (
                         <div style={{ width: 200 }} key={data.id}>
                             <div >
@@ -37,13 +44,13 @@ export default function Pagination() {
 
             <div style={{ display: "flex", justifyContent: "space-around" }}>
                 <button disabled={page === 1} style={{ cursor: page === 1 ? "not-allowed" : "pointer" }} onClick={() => setPage(page => page - 1)}>⬅️</button>
-                {[...Array(prodData.length / 5)].map((dd, i) => {
+                {[...Array(total)].map((dd, i) => {
                     return (
-                        <button style={{ backgroundColor: page === i + 1 && "pink", color: "red" }} onClick={() => handleData(i + 1)}>{i + 1}</button>
+                        <button key={i} style={{ backgroundColor: page === i + 1 && "pink", color: "red" }} onClick={() => handleData(i + 1)}>{i + 1}</button>
                     )
                 }
                 )}
-                <button style={{ cursor: prodData.length / 5 === page ? "not-allowed" : "pointer", display: prodData.length / 5 === page && "none" }} onClick={() => setPage(page => page + 1)}>➡️</button>
+                <button style={{ cursor: total === page ? "not-allowed" : "pointer", display: total === page && "none" }} onClick={() => setPage(page => page + 1)}>➡️</button>
             </div>
         </>
     )
